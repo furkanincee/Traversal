d46b8657ebb89fad5a087904617a5e94443a3d58
furkan <furkan@FURKAN5>
2023-10-20 20:34:28 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string Client { get; set; }
+        public string Comment{ get; set; }
+        public string ClientImage { get; set; }
+    }
+}
